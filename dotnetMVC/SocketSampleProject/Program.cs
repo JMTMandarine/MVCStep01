3e038b09e1f87a6e75afcf7db540349d4b2961c6
LTH <leeth4115@naver.com>
2019-05-05 15:44:25 +0900

    Socket Test

---
+++ dotnetMVC/SocketSampleProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketSampleProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        TcpListener server = null;
+        TcpClient clientSocket = null;
+        static int userCnt;
+        string date;
+        public Dictionary<TcpClient,string> clientList = new Dictionary<TcpClient,string>();
+    
+        public Form1()
+        {
+            InitializeComponent();
+            Thread t = new Thread(InitSocket);
+        }
+
+        private void InitSocket()
+        {
+            server = new TcpListener(IPAddress.Any, 8080);
+            clientSocket = default(TcpClient);
+            server.Start();
+            //DisplayText(">> Server Started");
+        }
+    }
+}
---
+++ dotnetMVC/socket/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.Mvc;
+
+namespace socket.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Socket mainSock;
+
+        public ActionResult Index()
+        {
+            try
+            {
+                // 소캣생성
+                mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                //주소바인딩 생성
+                IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 8080);
+                // 소캣에 바인딩
+                mainSock.Bind(serverEP);
+                // 클라이언트 요청 건수 10개가 되면 실행하게 설정
+                mainSock.Listen(10);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
